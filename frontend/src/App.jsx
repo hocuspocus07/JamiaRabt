8cfe9d79099faf032572cf914e22d433abe3799f
hocuspocus07 <150130202+hocuspocus07@users.noreply.github.com>
2025-04-18 16:47:44 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen w-screen bg-black'></div>
+  )
+}
+
+export default App
